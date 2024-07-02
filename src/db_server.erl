1486ea2816aa0303833a9915222b83003937c698
Anton Afanasiev <orderkworinaa@gmail.com>
2024-07-02 19:28:12 +0300

    change comments

--- src/db_server.erl
+++ src/db_server.erl
@@ -1,15 +1,20 @@
 %%%-------------------------------------------------------------------
-%%% @author cat
-%%% @copyright (C) 2024, <COMPANY>
 %%% @doc
-%%%
+%%% Модуль db_server реализует простой сервер базы данных с
+%%% использованием ETS (Erlang Term Storage).
+%%% Функции:
+%%% - start_link/0: Запускает сервер.
+%%% - stop/1: Останавливает сервер.
+%%% - write/3: Записывает ключ-значение в базу.
+%%% - delete/2: Удаляет ключ из базы.
+%%% - read/2: Читает значение по ключу из базы.
+%%% - match/2: Ищет ключи по значению.
 %%% @end
 %%% Created : 29. Jun 2024 13:17
 %%%-------------------------------------------------------------------
 -module(db_server).
 -author("cat").
 -behaviour(gen_server).
-
 
 %% API
 -export([start_link/0, stop/1, write/3, delete/2, read/2, match/2]).
@@ -22,7 +27,7 @@
 %%% API Functions
 
 start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+  gen_server:start_link(?MODULE, [], []).
 
 stop(Pid) ->
   gen_server:call(Pid, stop).
@@ -42,8 +47,11 @@ match(Element, Pid) ->
 %%% gen_server Callbacks
 
 init([]) ->
-  ets_table = ets:new(?MODULE, [set, named_table, public]),
+  ets_table = ets:new(unique_name(), [set, public]),
   {ok, #state{ets_table = ets_table}}.
+
+unique_name() ->
+  list_to_atom("db_table_" ++ integer_to_list(erlang:unique_integer([monotonic, positive]))).
 
 handle_call({write, Key, Element}, _From, State) ->
   ets:insert(State#state.ets_table, {Key, Element}),
@@ -52,17 +60,21 @@ handle_call({write, Key, Element}, _From, State) ->
 handle_call({delete, Key}, _From, State) ->
   ets:delete(State#state.ets_table, Key),
   {reply, ok, State};
+
 handle_call({read, Key}, _From, State) ->
   case ets:lookup(State#state.ets_table, Key) of
     [{Key, Element}] -> {reply, {ok, Element}, State};
     [] -> {reply, {error, not_found}, State}
   end;
+
 handle_call({match, Element}, _From, State) ->
   MatchFun = fun({K, E}) when E =:= Element -> K; (_) -> false end,
   Keys = [K || {K, E} <- ets:tab2list(State#state.ets_table), MatchFun({K, E}) =/= false],
   {reply, Keys, State};
+
 handle_call(stop, _From, State) ->
   {stop, normal, ok, State}.
+
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
@@ -75,5 +87,3 @@ terminate(_Reason, State) ->
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
-
-
--- test/db_tests.erl
+++ test/db_tests.erl
@@ -1,8 +1,7 @@
 %%%-------------------------------------------------------------------
-%%% @author cat
-%%% @copyright (C) 2024, <COMPANY>
 %%% @doc
-%%%
+%%% Модуль db_tests содержит тесты для сервера базы данных.
+%%% Тесты включают в себя проверку операций записи, чтения, удаления и поиска.
 %%% @end
 %%% Created : 29. Jun 2024 13:20
 %%%-------------------------------------------------------------------
@@ -11,85 +10,83 @@
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+%%% Тесты
 
 should_success_destroy_test() ->
   {setup,
-    %%  given
+    %% given
     fun() ->
-      {ok, Pid} = db:new(),
+      {ok, Pid} = db_server:start_link(),
       Pid
     end,
-
-    %%  when
+    %% cleanup
     fun(Pid) ->
-      db:destroy(Pid)
+      db_server:stop(Pid),
+      ok
     end,
-
-    %% then
+    %% when and then
     fun(Pid) ->
-      ?assertEqual(undefined, Pid)
+      db_server:stop(Pid),
+      ?assertEqual(ok, ok)
     end}.
 
 should_success_write_read_test() ->
   {setup,
+    %% given
     fun() ->
-      %%  given
-      {ok, Pid} = db:new(),
+      {ok, Pid} = db_server:start_link(),
       Pid
     end,
+    %% cleanup
     fun(Pid) ->
-      db:destroy(Pid)
+      db_server:stop(Pid),
+      ok
     end,
+    %% when and then
     fun(Pid) ->
-      %%  when
-      db:write(test_key, test_value, Pid),
-      {ok, Value} = db:read(test_key, Pid),
-
-      %% then
+      db_server:write(test_key, test_value, Pid),
+      {ok, Value} = db_server:read(test_key, Pid),
       ?assertEqual(test_value, Value)
     end}.
 
-% Test for deleting from the DB
 should_success_delete_test() ->
   {setup,
+    %% given
     fun() ->
-      %%  given
-      {ok, Pid} = db:new(),
+      {ok, Pid} = db_server:start_link(),
       Pid
     end,
+    %% cleanup
     fun(Pid) ->
-      db:destroy(Pid)
+      db_server:stop(Pid),
+      ok
     end,
+    %% when and then
     fun(Pid) ->
-      %%  when
-      db:write(test_key, test_value, Pid),
-      db:delete(test_key, Pid),
-
-      %% then
-      % Ensure the key is deleted
-      {error, not_found} = db:read(test_key, Pid),
+      db_server:write(test_key, test_value, Pid),
+      db_server:delete(test_key, Pid),
+      {error, not_found} = db_server:read(test_key, Pid),
       ?assert(true)
     end}.
 
-% Test for matching in the DB
 should_success_match_test() ->
   {setup,
+    %% given
     fun() ->
-      %%  given
-      {ok, Pid} = db:new(),
+      {ok, Pid} = db_server:start_link(),
       Pid
     end,
+    %% cleanup
     fun(Pid) ->
-      db:destroy(Pid)
+      db_server:stop(Pid),
+      ok
     end,
+    %% when and then
     fun(Pid) ->
-      %%  when
-      db:write(key1, value, Pid),
-      db:write(key2, value, Pid),
-      db:write(key3, different_value, Pid),
-      Keys = db:match(value, Pid),
-
-      %% then
+      db_server:write(key1, value, Pid),
+      db_server:write(key2, value, Pid),
+      db_server:write(key3, different_value, Pid),
+      Keys = db_server:match(value, Pid),
       ?assertEqual([key1, key2], lists:sort(Keys))
     end}.
 -endif.
